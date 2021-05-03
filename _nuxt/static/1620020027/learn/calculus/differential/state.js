f5e0ee05576e193b9337b99395c4212d669ce843
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-05-03 05:33:55 +0000

    deploy: 202cc3678fa095c8e78f5e93927a9466ebe7991a

---
+++ _nuxt/static/1620020027/learn/calculus/differential/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620020027",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fcalculus\u002Fdifferential",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0cc8c6e4"}}}}(null));
