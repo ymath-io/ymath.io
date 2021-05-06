2daa1ee6f65cd761b2c4e6c034bb7fe88b6be1a2
CoCowBubble <CoCowBubble@users.noreply.github.com>
2021-05-06 04:11:07 +0000

    deploy: e82eb940edb09af0c681ebadebbe381ec0ab3590

--- _nuxt/static/1620273790/learn/exponents/properties/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620273790",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fexponents\u002Fproperties",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7fa47cef"}}}}(null));
