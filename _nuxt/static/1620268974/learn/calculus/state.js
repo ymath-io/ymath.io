00da3c28d3e0d5ce35cc7b26fa03c24f1b05e1a3
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-05-06 02:43:05 +0000

    deploy: 9f6aa6bfaee7e065414701afae1d7ba6a7b4217d

---
+++ _nuxt/static/1620268974/learn/calculus/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620268974",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fcalculus",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f73ff123"}}}}(null));
