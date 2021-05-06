e048d52f3ce0bb9be66133eb416bf385f0ebf3de
CoCowBubble <CoCowBubble@users.noreply.github.com>
2021-05-06 04:50:26 +0000

    deploy: fc891b3bda2c6925bf345b626242d6d1ee051088

--- _nuxt/static/1620276536/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620276536",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e94894b9"}}}}(null,"\u002F"));
