e9ac11f7c991148367374f24ec7c3fe2dd8bc97a
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-05-07 17:18:58 +0000

    deploy: 278d98f1149b5d2b3918a23140ba94eae1e12d73

--- _nuxt/static/1620407853/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620407853",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"65db76cb"}}}}(null,"\u002F"));
