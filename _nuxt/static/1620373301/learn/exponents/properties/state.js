4b75dec30b4c8e6cd4a73d5de328d2721b535e33
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-05-07 07:42:08 +0000

    deploy: ee016fd6ca5d6d95812135e12c31e886f6ffff53

--- _nuxt/static/1620373301/learn/exponents/properties/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620373301",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fexponents\u002Fproperties",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7659af52"}}}}(null));
