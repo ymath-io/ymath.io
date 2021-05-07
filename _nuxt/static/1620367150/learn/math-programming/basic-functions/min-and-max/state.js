12e151e4aaaad81eba069592e375dfc46e4ecfc5
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-05-07 05:59:17 +0000

    deploy: eaf345b0287e245dce501d4dcf3d2ad1a79fee62

---
+++ _nuxt/static/1620367150/learn/math-programming/basic-functions/min-and-max/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620367150",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fmath-programming\u002Fbasic-functions\u002Fmin-and-max",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a025ceea"}}}}(null));
