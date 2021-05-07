2a08181321afe0a346603c55e94e8822c234f3a7
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-05-07 07:33:52 +0000

    deploy: 4f3512bdf882837fa76332b38557f89be7c9429b

--- _nuxt/static/1620372751/learn/exponents/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620372751",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fexponents",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fac4187f"}}}}(null));
---
+++ _nuxt/static/1620372824/learn/calculus/differential/introduction/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620372824",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fcalculus\u002Fdifferential\u002Fintroduction",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9c76bce3"}}}}(null));
