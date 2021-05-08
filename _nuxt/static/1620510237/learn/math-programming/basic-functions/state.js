827054c1a8f28312e6788ed1214c028ae0528ad0
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-05-08 21:45:28 +0000

    deploy: afd5caa2fb628d6da77ef22b857393fde95a1253

--- _nuxt/static/1620510237/learn/math-programming/basic-functions/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620510237",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fmath-programming\u002Fbasic-functions",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7d9f1f6c"}}}}(null));
---
+++ _nuxt/static/1620510321/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620510321",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f1e57829"}}}}(null,"\u002F"));
