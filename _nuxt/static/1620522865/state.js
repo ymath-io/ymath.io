49940098e2bb0f728d48b9b50571d5f5ba6bec5c
sophiazhu16 <sophiazhu16@users.noreply.github.com>
2021-05-09 01:16:08 +0000

    deploy: bea5c9b27258298bd5bf4828cd8fc81ad0dfb40a

--- _nuxt/static/1620522865/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620522865",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"36cd1c1d"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1620522960/learn/exponents/powers/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620522960",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fexponents\u002Fpowers",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e9c6b837"}}}}(null));
