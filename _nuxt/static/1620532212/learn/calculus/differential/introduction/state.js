1afabe36d2d9a1afb5a228b62f04a2a50e8daf09
sophiazhu16 <sophiazhu16@users.noreply.github.com>
2021-05-09 03:51:54 +0000

    deploy: e0a06a4e48b86d97f604cf6a4a9936c1a3209837

--- _nuxt/static/1620532212/learn/calculus/differential/introduction/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620532212",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fcalculus\u002Fdifferential\u002Fintroduction",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"98cf1f28"}}}}(null));
--- _nuxt/static/1620532212/learn/exponents/exponents-basic/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620532212",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fexponents\u002Fexponents-basic",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"98cf1f28"}}}}(null));
