0fc5eaa725bd79860a9dc8c2666d92e2bd587b60
sophiazhu16 <sophiazhu16@users.noreply.github.com>
2021-05-09 01:37:33 +0000

    deploy: 35f3738c17bbe2b9efd5925d79f70151b4efb39c

---
+++ _nuxt/static/1620524244/learn/math-programming/basic-functions/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620524244",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fmath-programming\u002Fbasic-functions",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"11111cc4"}}}}(null));
