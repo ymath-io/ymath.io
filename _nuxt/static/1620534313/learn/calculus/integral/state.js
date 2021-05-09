f070b2a96b1b8797f71ab7a1df0ba7ef92e3003c
sophiazhu16 <sophiazhu16@users.noreply.github.com>
2021-05-09 04:26:39 +0000

    deploy: 9b57b29998071927419fa17a3dc8f28629481f08

--- _nuxt/static/1620534313/learn/calculus/integral/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620534313",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fcalculus\u002Fintegral",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3a23df6d"}}}}(null));
