1261222fc3650af64c86117c08043fdbd75d3775
sophiazhu16 <sophiazhu16@users.noreply.github.com>
2021-05-09 01:32:54 +0000

    deploy: ce4d789e37da8b839da20e776d211eba5b75660c

---
+++ _nuxt/static/1620523965/learn/math-programming/basic-functions/absolute-value/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620523965",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fmath-programming\u002Fbasic-functions\u002Fabsolute-value",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"823cdc6f"}}}}(null));
