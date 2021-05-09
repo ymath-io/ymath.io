b4f0dced7ec86c5a10cf4c2206e7fbe5e017ef4c
sophiazhu16 <sophiazhu16@users.noreply.github.com>
2021-05-09 01:41:55 +0000

    deploy: ffe9ef3c15f37a9b86ce9bb7a62705f10210fa2e

--- _nuxt/static/1620524428/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620524428",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a7706f7d"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1620524508/learn/exponents/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620524508",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fexponents",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bb04f363"}}}}(null));
