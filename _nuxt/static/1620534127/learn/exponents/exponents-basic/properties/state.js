5107ae526cca99c5b3c0be5f51f87f1d4d809855
sophiazhu16 <sophiazhu16@users.noreply.github.com>
2021-05-09 04:22:16 +0000

    deploy: af2e16db9642fe165fbb3bede0b2f3278a622b7c

---
+++ _nuxt/static/1620534127/learn/exponents/exponents-basic/properties/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620534127",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fexponents\u002Fexponents-basic\u002Fproperties",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1a671485"}}}}(null));
