f49d8d7644eb074fa7ad999da9fd83ec96957f9f
sophiazhu16 <sophiazhu16@users.noreply.github.com>
2021-05-09 03:50:19 +0000

    deploy: 245c892300229fe6d9d6eb7d6ec3042732bb96e6

---
+++ _nuxt/static/1620532212/test/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620532212",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftest",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"98cf1f28"}}}}(null));
