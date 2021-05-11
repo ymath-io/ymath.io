f47a34c806acb2b25a0e6f2057c8dbf89979698b
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-05-11 09:23:09 -0700

    style: align MATH in logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,5 @@
 <template>
   <svg preserveAspectRatio :height='size||100' viewBox='0 0 400 400'>
-
     <rect x="0" y="0" rx="60" ry="60" width="400" height="400"
           style="fill:#00ab33;stroke-width:5;opacity:1" />
 
@@ -22,7 +21,7 @@
         font-size="75px"
         transform-box="fill-box" transform-origin="center"
         transform="rotate(-67)
-   skewX(22.5)
+   skewX(23.5)
     scale(1.06 1)"
         font-family="Lato">
         MATH
