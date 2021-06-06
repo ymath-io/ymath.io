e81925e8d521ecf03e2c050c4fdb890e099b6106
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-06-05 20:22:43 -0700

    style: actually add dark mode to library

--- pages/library/index.vue
+++ pages/library/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <container v-if='papers'>
-      <div class='bg-white p-3 rounded-lg my-4 shadow-md hover:shadow-2xl transition duration-200 relative' :key='paper.slug' v-for='paper of papers'>
-        <a :href='`/library/${paper.slug}`' class='font-bold text-gray-800 stretched-link text-lg'>{{paper.title}}</a>
-        <p><span class='text-green-400 font-semibold'>{{paper.author}}</span> <span>on {{$moment(paper.date).format('MMMM do YYYY')}}</span></p>
-        <p class='text-gray-500'>
+      <div class='bg-white p-3 dark:bg-gray-800 dark:hover:bg-gray-700 rounded-lg my-4 shadow-md hover:shadow-2xl transition duration-200 relative' :key='paper.slug' v-for='paper of papers'>
+        <a :href='`/library/${paper.slug}`' class='font-bold text-gray-800 dark:text-gray-300 stretched-link text-lg'>{{paper.title}}</a>
+        <p><span class='text-green-400 font-semibold'>{{paper.author}}</span> <span class='dark:text-darkText'>on {{$moment(paper.date).format('MMMM do YYYY')}}</span></p>
+        <p class='text-gray-500 dark:text-gray-400'>
           {{paper.description}}
         </p>
       </div>
