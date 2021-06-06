928f0bf0fdda5c741e214651d3b93f593e028093
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-06-06 00:45:03 +0000

    deploy: 157ac138a75eacf048ae0e8cec240a5b61424659

--- _nuxt/static/1622914945/learn/math-programming/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622914945",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fmath-programming",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dfd5e9f6"}}}}(null));
--- _nuxt/static/1622914945/practice/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/practice", {data:[{}],fetch:{},mutations:void 0});
