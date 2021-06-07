19300b220b8c614c9357d7966266c09e2137d8db
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-06-06 17:46:15 -0700

    fix: use day of month, not week for library

--- pages/library/index.vue
+++ pages/library/index.vue
@@ -3,7 +3,7 @@
     <container v-if='papers'>
       <div class='bg-white p-3 dark:bg-gray-800 dark:hover:bg-gray-700 rounded-lg my-4 shadow-md hover:shadow-2xl transition duration-200 relative' :key='paper.slug' v-for='paper of papers'>
         <a :href='`/library/${paper.slug}`' class='font-bold text-gray-800 dark:text-gray-300 stretched-link text-lg'>{{paper.title}}</a>
-        <p><span class='text-green-400 font-semibold'>{{paper.author}}</span> <span class='dark:text-darkText'>on {{$moment(paper.date).format('MMMM do YYYY')}}</span></p>
+        <p><span class='text-green-400 font-semibold'>{{paper.author}}</span> <span class='dark:text-darkText'>on {{$moment(paper.date).format('MMMM Do YYYY')}}</span></p>
         <p class='text-gray-500 dark:text-gray-400'>
           {{paper.description}}
         </p>
