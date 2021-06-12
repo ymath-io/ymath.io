fe43a84d6941ddace39f88b59adf9519a66f6e8a
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-06-12 23:04:12 +0000

    deploy: a9fe94b27a1244878277571822566fc19a560589

--- _nuxt/static/1623085175/library/on-the-topic-of-fibonacci-like-sequences/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623085175",layout:"default",error:a,serverRendered:true,routePath:"\u002Flibrary\u002Fon-the-topic-of-fibonacci-like-sequences",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c31e7324"}}}}(null));
---
+++ _nuxt/static/1623539035/learn/physics-c-mechanics/rotational-motion/centripetal-acceleration/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623539035",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fphysics-c-mechanics\u002Frotational-motion\u002Fcentripetal-acceleration",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"03d328a8"}}}}(null));
