9a478954c1debbae85601bc2739ca1a0964996ca
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-06-23 02:57:16 +0000

    deploy: 02bf4e85743c6a84b18208ef0db148ca04984b28

---
+++ _nuxt/static/1624417019/learn/physics-c-mechanics/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624417019",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fphysics-c-mechanics",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"52be7a1d"}}}}(null));
