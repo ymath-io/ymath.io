41bed606e7b9f93543ff0ffaa1d9785917c966d1
Saumya Singhal <65318685+saumyasinghal747@users.noreply.github.com>
2021-06-22 19:15:25 -0700

    fix: lighten dark mode author names
    Fixes #54

--- pages/library/_paper.vue
+++ pages/library/_paper.vue
@@ -2,7 +2,7 @@
   <container>
     <div v-if='paper'>
       <h1 class='mb-3'><span class='text-green-400 font-semibold text-2xl'>{{ paper.title }}</span> <span
-        class='text-xl dark:text-gray-500 text-gray-700'> written by {{ paper.author }}</span></h1>
+        class='text-xl dark:text-gray-400 text-gray-700'> written by {{ paper.author }}</span></h1>
       <embed class='w-full' style='height: 38rem' :src='paper.embed.link' />
       <a class='text-green-400 font-bold my-2 block' target='_blank' :href='paper.download'>Download
         {{ paper.embed.type }}</a>
