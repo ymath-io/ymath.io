c6d6fa3c92de8cd18e005bef4bfca26263debebf
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-06-26 18:35:00 -0700

    content: home page stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <div class='max-w-7xl mt-8 mx-10 gap-8 py-6 px-4 sm:px-6 lg:px-8 mx-auto  lg:grid grid-cols-4'>
-      <div class='grid grid-cols-3 section'>
-        <div class='hero-image col-span-1'>
+    <div class=''>
+      <div :class="{'bg-green-400 bg-opacity-20':section.type==='bubble'}" v-for='section of doc.sections' class='grid  my-5   grid-cols-3 '>
+        <div class='hero-image border border-dashed border-red-800 col-span-1 ml-32'>
 
         </div>
-        <div class='col-span-2'>
-          <h1 class='underline-green-500'>Redefining Learning.</h1>
+        <div class='col-span-2 pl-16  text-right pr-16 mr-32 border border-dashed border-yellow-900'>
+          <h1 class='text-4xl my-5  font-bold font-sans'>{{section.title}}</h1>
+          <p class='text-xl dark:text-darkText leading-loose my-3'>{{section.body}}</p>
+          <div class="my-5" v-if='section.button'>
+            <a class="bg-green-400  text-white  cursor-pointer hover:bg-green-500 font-bold inline-flex py-2 px-3 rounded-lg  shadow-lg" :href='button.href' v-for='button of [section.button]'>
+              {{button.title}}
+            </a>
+          </div>
+
         </div>
       </div>
 
@@ -21,8 +28,13 @@ import Vue from 'vue'
 export default Vue.extend({
   components: {},
   data: () => ({}),
+  async asyncData({ $content }) {
+    const doc = await $content('home/index').fetch();
+    return {doc}
+  },
   head: {
-    title: 'Home'
+    title: 'YMath.io - A New Way to Learn Math',
+    titleTemplate: null
   }
 })
 </script>
