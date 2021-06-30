c292ed3f367d7f397af781803fa30e37e8a314bf
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-06-30 08:52:03 -0700

    feat: all the images are there

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class='overflow-x-hidden'>
-      <div :class="{'bg-green-400 bg-opacity-20':section.type==='bubble'}" v-for='section of [doc.sections[0]]' class='grid  my-5 py-24  grid-cols-3 '>
+      <div :class="[section.type==='bubble'? bubbleClasses:'', sectionClasses]" v-for='section of [doc.sections[0]]' :style='section.type==="bubble"?bubbleStyle:{}'   >
         <div class='col-span-2 pl-16  text-right pr-16 ml-32'>
           <h1 class='text-4xl  dark:text-darkText my-5 underline-green-500 font-bold font-sans'>{{section.title}}</h1>
           <p class='text-xl  dark:text-darkText leading-loose my-3'>{{section.body}}</p>
@@ -16,7 +16,7 @@
           <img  style='transform: scaleX(-1);' class='my-auto transform  mx-auto' src='/img/mascot/flying.png' />
         </div>
       </div>
-      <div :class="{'bg-green-400 bg-opacity-20':section.type==='bubble'}" v-for='section of [doc.sections[1]]' style='border-top-right-radius: 50vw 15vw;border-top-left-radius: 50vw 15vw;border-bottom-left-radius: 50vw 15vw;border-bottom-right-radius: 50vw 15vw' class='grid -mx-5 my-5 py-52 grid-cols-3 '>
+      <div :class="[section.type==='bubble'? bubbleClasses:'', sectionClasses]" v-for='section of [doc.sections[1]]' :style='section.type==="bubble"?bubbleStyle:{}' >
         <div class='hero-image flex col-span-1 ml-32'>
           <img class='my-auto transform  mx-auto' src='/img/mascot/writing.png' />
         </div>
@@ -32,7 +32,7 @@
         </div>
 
       </div>
-      <div :class="{'bg-green-400 bg-opacity-20':section.type==='bubble'}" v-for='section of [doc.sections[2]]' class='grid  my-5 py-24  grid-cols-3 '>
+      <div :class="[section.type==='bubble'? bubbleClasses:'', sectionClasses]" v-for='section of [doc.sections[2]]' :style='section.type==="bubble"?bubbleStyle:{}'>
 
         <div class='col-span-2 pl-16  text-right pr-16 ml-32 '>
           <h1 class='text-4xl my-5 dark:text-darkText font-bold font-sans'>{{section.title}}</h1>
@@ -47,7 +47,7 @@
           <img class='my-auto transform bg-green-400 bg-opacity-20 p-5 rounded-full mx-auto' src='/img/mascot/notebook.png' />
         </div>
       </div>
-      <div :class="{'bg-green-400 bg-opacity-20 -mx-5 my-5 py-52':section.type==='bubble'}" v-for='section of [doc.sections[3]]' class='grid  my-5   grid-cols-3 ' style='border-top-right-radius: 50vw 15vw;border-top-left-radius: 50vw 15vw;border-bottom-left-radius: 50vw 15vw;border-bottom-right-radius: 50vw 15vw'   >
+      <div :class="[section.type==='bubble'? bubbleClasses:'', sectionClasses]" v-for='section of [doc.sections[3]]' :style='section.type==="bubble"?bubbleStyle:{}'  >
         <div class='hero-image  col-span-1 ml-32'>
             <div class='relative'>
               <img style='z-index:0' class='absolute transform scale-125 top-0' src='/img/mascot/scenes/code-wall.png'/>
@@ -55,22 +55,21 @@
 
             </div>
         </div>
-        <div class='col-span-2 pl-16  text-center pr-16 mr-32'>
-          <h1 class='text-4xl my-5 dark:text-darkText font-bold font-sans'>{{section.title}}</h1>
+        <div class='col-span-2 pl-16 flex text-center pr-16 mr-32'>
+
+          <div class='my-auto'><h1 class='text-4xl my-5 dark:text-darkText font-bold font-sans'>{{section.title}}</h1>
           <p class='text-xl dark:text-darkText leading-loose my-3'>{{section.body}}</p>
           <div class="my-5 " v-if='section.button'>
             <a class="bg-green-500  text-white  cursor-pointer hover:bg-green-500 font-bold inline-flex py-2 px-3 rounded-lg  shadow-lg" :href='button.href' v-for='button of [section.button]'>
               {{button.title}}
             </a>
-          </div>
+          </div></div>
 
         </div>
       </div>
-      <div :class="{'bg-green-400 bg-opacity-20':section.type==='bubble'}" v-for='section of [doc.sections[4]]' class='grid  my-5   grid-cols-3 '>
-        <div class='hero-image border border-dashed border-red-800 col-span-1 ml-32'>
+      <div :class="[section.type==='bubble'? bubbleClasses:'', sectionClasses]" v-for='section of [doc.sections[4]]' >
 
-        </div>
-        <div class='col-span-2 pl-16  text-right pr-16 mr-32 border border-dashed border-yellow-900'>
+        <div class='col-span-2 px-16  text-right ml-32 '>
           <h1 class='text-4xl my-5 dark:text-darkText font-bold font-sans'>{{section.title}}</h1>
           <p class='text-xl dark:text-darkText leading-loose my-3'>{{section.body}}</p>
           <div class="my-5" v-if='section.button'>
@@ -79,6 +78,13 @@
             </a>
           </div>
 
+        </div>
+        <div class='hero-image col-span-1 mr-32'>
+          <div class='relative'>
+            <img style='z-index:0' class='absolute transform scale-125 top-0' src='/img/mascot/scenes/speech.png'/>
+            <img style='z-index: 1' class='relative transform scale-75' src='/img/mascot/side-view.png'/>
+
+          </div>
         </div>
       </div>
 
@@ -92,7 +98,11 @@ import Vue from 'vue'
 
 export default Vue.extend({
   components: {},
-  data: () => ({}),
+  data: () => ({
+    bubbleClasses:'bg-green-400 bg-opacity-20 px-12  -mx-12 py-52',
+    sectionClasses:'grid gap-4 my-5 py-24 grid-cols-3 ',
+    bubbleStyle: {borderRadius:'50vw / 15vw'}
+  }),
   async asyncData({ $content }) {
     const doc = await $content('home/index').fetch();
     return {doc}
