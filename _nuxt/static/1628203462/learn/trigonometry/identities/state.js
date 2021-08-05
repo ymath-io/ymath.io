5477bd3a2a1bc63dfcedf1fa2192545b0091ff9b
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-08-05 22:44:43 +0000

    deploy: 40aeeb9bbdb86ec12bc02c3d8d494b5d4d226131

---
+++ _nuxt/static/1628203462/learn/trigonometry/identities/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628203462",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Ftrigonometry\u002Fidentities",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a41899bd"}}}}(null));
