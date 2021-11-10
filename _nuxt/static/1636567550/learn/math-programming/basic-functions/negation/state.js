178b3804fb8592147ce0f96cae2fe7db575dc2e1
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-11-10 18:06:08 +0000

    deploy: 737c4deca4485328574f64ec0b58c66a86affdbb

---
+++ _nuxt/static/1636567550/learn/math-programming/basic-functions/negation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636567550",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fmath-programming\u002Fbasic-functions\u002Fnegation",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a51a4f45"}}}}(null));
