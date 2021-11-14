a443ae6863c7d443f1996139d2c28130f5e0726b
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-11-14 02:15:14 +0000

    deploy: cccf2709897bb456ad959662b6e8dfd0c2739c90

--- _nuxt/static/1636855823/learn/trigonometry/identities/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636855823",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Ftrigonometry\u002Fidentities",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"90a485e2"}}}}(null));
---
+++ _nuxt/static/1636856093/index/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636856093",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"a5834798"}}}}("\u002Findex"));
