bd9aba7916e86522fb3e602f878e5e890027a7cb
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-11-14 02:10:43 +0000

    deploy: fcde9c93565b9558359a1b55a83e5834b57252aa

--- _nuxt/static/1636855781/index/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636855781",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"554fbd08"}}}}("\u002Findex"));
