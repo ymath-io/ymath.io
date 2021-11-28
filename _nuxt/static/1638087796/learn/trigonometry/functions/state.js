9f5f25453b7493423bc7ed68b1884f556cc69812
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-11-28 23:09:13 +0000

    deploy: 2432a494c91b648c0fa5bd75b0ac3f83927eb7f8

--- _nuxt/static/1638087796/learn/trigonometry/functions/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638087796",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Ftrigonometry\u002Ffunctions",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"09ad8b89"}}}}(null));
---
+++ _nuxt/static/1638140937/learn/calculus/integral/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638140937",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fcalculus\u002Fintegral",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a3bcc2c0"}}}}(null));
