71ea1a6033095c39e06760ea02bc3571f86bef20
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-11-28 06:55:42 +0000

    deploy: 36f99f991f7668d4b65c340aad45a9057218347e

--- _nuxt/static/1638082245/learn/calculus/differential/power-rule/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638082245",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fcalculus\u002Fdifferential\u002Fpower-rule",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6c759f35"}}}}(null));
---
+++ _nuxt/static/1638082518/learn/trigonometry/identities/law-of-sines/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638082518",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Ftrigonometry\u002Fidentities\u002Flaw-of-sines",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"81848b45"}}}}(null));
