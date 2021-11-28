e334872905735324e51aa42f0ab46f2429f15e9d
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-11-28 08:23:36 +0000

    deploy: cec34614a162e978d5c4a1df860106fb392e5ebd

---
+++ _nuxt/static/1638087796/practice/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638087796",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpractice",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"09ad8b89"}}}}(null));
