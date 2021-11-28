9d355af379c5f92145bbee35d6f147f06b2e3b69
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-11-28 06:56:10 +0000

    deploy: 35e24403032367f9cea8dac87811257af401c154

--- _nuxt/static/1638082518/learn/exponents/exponents-basic/introduction/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638082518",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fexponents\u002Fexponents-basic\u002Fintroduction",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"81848b45"}}}}(null));
