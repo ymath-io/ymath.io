665a0908da250e7d88e40da79f663aad37be3406
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-11-28 06:51:06 +0000

    deploy: a56e64f8698ccc579bcea171a61f26ad8fc49dd6

--- _nuxt/static/1638080776/learn/physics-c-mechanics/rotational-motion/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638080776",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fphysics-c-mechanics\u002Frotational-motion",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"023ec35c"}}}}(null));
---
+++ _nuxt/static/1638082245/learn/calculus/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638082245",layout:"default",error:a,serverRendered:true,routePath:"\u002Flearn\u002Fcalculus",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6c759f35"}}}}(null));
