7541827cf9c012e0a778239ec7a7a9ab0a71fe65
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2022-01-11 07:43:22 +0000

    deploy: 382f48b64637a04e3895fbd0c55e8954065873e0

---
+++ _nuxt/static/1641886988/library/rotating-a-parabola/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641886988",layout:"default",error:a,serverRendered:true,routePath:"\u002Flibrary\u002Frotating-a-parabola",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"026f117c"}}}}(null));
